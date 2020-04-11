64f793458391a33980bfba5d91014a44d43a87d8
Azax4 <47921970+Azax4@users.noreply.github.com>
2020-04-11 19:16:51 -0400

    Update proj.py

--- proj.py
+++ proj.py
@@ -10,9 +10,8 @@ for line in f:
         line = line.replace("\n","")
         t = line.split("\t")
         if t[0] != t[1]:
-            # x = (np.random.rand()%10)/10
-            G.add_edge(t[0],t[1],weight = 1)
-T = nx.algorithms.approximation.steinertree.metric_closure(G)
-print(nx.Graph.size(T))
-# for j in list(G.edges()):
-    # print(nx.shortest_path(G, source = j[0], target = j[1]))
+            x = (np.random.rand()%10)/10
+            G.add_edge(t[0],t[1],weight = x)
+print(len(nx.minimum_edge_cut(G)))
+# T = nx.algorithms.approximation.steinertree.metric_closure(G)
+# print(nx.Graph.size(T))
