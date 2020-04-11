83b81c4c49a9d745fa381c65ae43065107159b1d
Azax4 <47921970+Azax4@users.noreply.github.com>
2020-04-11 19:11:14 -0400

    Update proj.py

--- proj.py
+++ proj.py
@@ -1,5 +1,6 @@
+import networkx.algorithms.approximation
 import networkx as nx
-
+import numpy as np
 f = open("as19971108.txt","r")
 G = nx.Graph()
 i =  0
@@ -9,10 +10,9 @@ for line in f:
         line = line.replace("\n","")
         t = line.split("\t")
         if t[0] != t[1]:
-            G.add_edge(t[0],t[1],weight = 0)
-T1 = nx.minimum_spanning_tree(G, algorithm = "prim")
-T2 = nx.minimum_spanning_tree(G, algorithm = "kruskal")
-Gc = nx.Graph()
+            # x = (np.random.rand()%10)/10
+            G.add_edge(t[0],t[1],weight = 1)
+T = nx.algorithms.approximation.steinertree.metric_closure(G)
+print(nx.Graph.size(T))
 # for j in list(G.edges()):
-    # print(nx.shortest_path(T, source = j[0], target = j[1]))
-print(len(list(T1.edges())))
+    # print(nx.shortest_path(G, source = j[0], target = j[1]))
