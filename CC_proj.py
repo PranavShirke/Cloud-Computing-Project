c685ce47207ac12bed1ea9c70685e4741b660c73
Pranavs05 <32169690+Pranavs05@users.noreply.github.com>
2020-04-11 19:18:33 -0400

    Update CC_proj.py

--- CC_proj.py
+++ CC_proj.py
@@ -1,15 +1,22 @@
 import networkx as nx
+import random
 G =nx.Graph()
+S = nx.Graph()
 def main():
 
 
 		with open("test.txt","r") as fp:
 			line = fp.readline()
-	  
+	 		
 			while line:
 				a=line.split()
 				line = fp.readline()
 				G.add_edge(a[0],a[1])
-		T=nx.minimum_spanning_tree(G)
+		
+
+		print(len(nx.minimum_edge_cut(G)))
+
+	
+ 
 if __name__ == '__main__':
     main()
