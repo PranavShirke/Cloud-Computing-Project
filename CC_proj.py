da35cb5a74ad132de94fbbf634c40b01e8951381
Pranavs05 <32169690+Pranavs05@users.noreply.github.com>
2020-04-12 15:50:28 -0400

    Mutation added

--- CC_proj.py
+++ CC_proj.py
@@ -1,8 +1,10 @@
 import networkx as nx
 import random
 import numpy as np
-G =nx.Graph()
-S = nx.Graph()
+G1 =nx.Graph()
+G2 = nx.Graph()
+G3 = nx.Graph()
+G4 = nx.Graph()
 def main():
 
 
@@ -14,21 +16,66 @@ def main():
 				a=line.split("\t")
 				line = fp.readline()
 				x = (np.random.rand()%10)/10
-				G.add_edge(a[0],a[1],weight=x)
+				y = (np.random.rand()%9)/10
+				G1.add_edge(a[0],a[1],weight=x)
+				G2.add_edge(a[0],a[1],weight=x)
+
 		
-		sum=0
-		mdegree =0
-		for j in list(G.nodes()):
-			sum+=G.degree(j)
-			if G.degree(j)>mdegree:
-				mdegree=G.degree(j)
+		sum1=0
+		mdegree1 =0
+		sum2=0
+		mdegree2=0
+		for j in list(G1.nodes()):
+			sum1+=G1.degree(j)
+			if G1.degree(j)>mdegree1:
+				mdegree1=G1.degree(j)
+		for j in list(G2.nodes()):
+			sum2+=G2.degree(j)
+			if G2.degree(j)>mdegree2:
+				mdegree2=G2.degree(j)
 													#Degree of a node 
-		avgdegree= sum/len(list(G.nodes()))
-		print(avgdegree)							#avg degree
-		print(mdegree)								#max degree
+		avgdegree1= sum1/len(list(G1.nodes()))
 
-		print(G.size(weight='weight'))					#Total cost of network
+		#print(avgdegree1)							#avg degree
+		#print(mdegree1)								#max degree
 
+		print(G1.size(weight='weight'))					#Total cost of network
+		print(G2.size(weight='weight'))
+		def crossover (G1,G2):
+
+				t1 = list(G1.edges())
+				t2 = list(G2.edges())
+				x = random.randint(0,len(t1))
+				for j in range(x):
+					#print(G1.get_edge_data(*t1[j])['weight'])
+					G3.add_edge(*t1[j],weight = G1.get_edge_data(*t1[j])['weight'])	
+
+				z = x
+				#print(x)
+				for k in range(len(t2)):
+						if z == len(t1):
+							break
+						else:
+							if t2[k] not in t1 : 
+								G3.add_edge(*t2[k], weight = G1.get_edge_data(*t2[k])['weight'])
+						z=z+1
+
+				z = x
+
+				for j in range(len(t1)-x):
+					G4.add_edge(*t1[j],weight = G1.get_edge_data(*t1[j])['weight'])
+
+				for k in range(len(t2)-x):
+						if z == len(t1):
+							break
+						else:
+							if t2[k] not in t1 : 
+								G4.add_edge(*t2[k], weight = G1.get_edge_data(*t2[k])['weight'])
+						z=z+1
+
+		crossover(G1,G2)			
+		print(G3.size(weight='weight'))
+		print(G4.size(weight='weight'))
 		
 	
  
