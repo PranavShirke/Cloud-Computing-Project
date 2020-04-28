2fc569c05eb7d982b9ab368bd832c83bef6277c3
Pranavs05 <32169690+Pranavs05@users.noreply.github.com>
2020-04-28 18:07:37 -0400

    Update CC_proj.py

--- CC_proj.py
+++ CC_proj.py
@@ -18,7 +18,7 @@ def crossover (G1,G2):
         x = len(t1)
     for j in range(x):
         #print(G1.get_edge_data(*t1[j])['weight'])
-        Gc1.add_edge(*t1[j],weight = G1.get_edge_data(*t1[j])['weight'])	
+        Gc1.add_edge(*t1[j],weight = G1.get_edge_data(*t1[j])['weight'])    
 
     z = x
     #print(x)
@@ -49,13 +49,6 @@ def crossover (G1,G2):
 
 
 
-def fillin(Gc1):
-    c1 = list(nx.isolates(Gc1))
-    Gc1.remove_nodes_from(nx.isolates(Gc1))
-    for i in range(len(c1)):
-        random_node = choice(Gc1.nodes())
-        Gc1.add_edge(random_node,c1[i]) 
-    return Gc1
 
 
 def add_new_node(G):
@@ -90,6 +83,13 @@ def avg_degree(G):
     else:
         return 0
     
+def normalize(scoret):
+    tmax = np.max(scoret, axis = 0)
+    score = []
+    for i in scoret:
+        score.append((i[0]/tmax[0])/((i[1]/tmax[1])+(i[2]/tmax[2])+(i[3]/tmax[3]))+(i[4]/tmax[4]))
+    return score
+
 
 
 
@@ -116,6 +116,31 @@ def mutation(G):
     return G
 
 @timeout_decorator.timeout(5, timeout_exception=FunctionTimedOut)
+
+def mutation2(G):
+    t = list(G.nodes())
+    td1 = np.random.randint(0, len(t))
+    td2 = np.random.randint(0, len(t))
+    l1 = list(G.edges(t[td1]))
+    l2 = list(G.edges(t[td2]))
+    for item in l1:
+        x = np.random.randint(0, 2)
+        if x % 2 == 0:
+            w =  G.get_edge_data(item[0], item[1])['weight']
+            G.remove_edge(item[0],item[1])
+            G.add_edge(t[td2],item[1], weight = w)
+    for item in l2:
+        x = np.random.randint(0, 2)
+        if x % 2 == 0:
+            if  G.has_edge(item[0], item[1])==True:
+                w =  G.get_edge_data(item[0], item[1])['weight']
+                G.remove_edge(item[0],item[1])
+                G.add_edge(t[td1],item[1], weight = w)
+
+    return G
+
+
+@timeout_decorator.timeout(5, timeout_exception=FunctionTimedOut)
 def min_cut_edge(G):
     return len(nx.minimum_edge_cut(G))
 
@@ -124,6 +149,11 @@ def min_cut_edge(G):
 def time_delay(G):
          T = nx.algorithms.approximation.steinertree.metric_closure(G)
          return nx.Graph.size(T)
+
+
+@timeout_decorator.timeout(5, timeout_exception=FunctionTimedOut)
+def get_size(G):
+    return G.size(weight='weight')
 
 
     #T = nx.algorithms.approximation.steinertree.metric_closure(G,weight='weight')
@@ -144,29 +174,19 @@ def fitness(x):
         except FunctionTimedOut:
             g = 15
             print(g)
-        s=(f/(max_degree(x)+avg_degree(x))+g)
+
+        try:
+            m = get_size(x)
+            print(m)
+        except FunctionTimedOut:
+            m = 10
+            print(m)
+
+        #s=(f/(max_degree(x)+avg_degree(x))+g+m)
         #print((max_degree(x)+avg_degree(x)))
-        return (s)#+min_cut_edge(x,) 
+        return ([f,g,max_degree(x),avg_degree(x),m])#+min_cut_edge(x,) 
     else:
         return 1 #(
-
-mutation_rate = 5 #mutation rate
-n = 9 #number of genes
-niter = 10
-genes = []
-f = open("test.txt","r")
-G = nx.Graph()
-i =  0
-for line in f:
-    i = i+1
-    if i > 4:
-        line = line.replace("\n","")
-        t = line.split("\t")
-        if t[0] != t[1]:
-            # x = (np.random.rand()%10)/10
-            G.add_edge(t[0],t[1],weight = 1)
-
-#generate initial population
 
 mutation_rate = 5
 n = 9 #number of genes
@@ -189,7 +209,7 @@ for line in f:
 
 genes.append(G)
 t = mutation(G)
-print(n)
+# print(n)
 for i in range(n):
     while(nx.is_connected(t)==False):
         t = mutation(t)
@@ -199,26 +219,25 @@ for i in range(n):
 n = n+1
 # genes = [1,2,3,4,5,6,7,7,8,10]
 score = []
+scoret = []
 for i in range(n):
-    score.append(fitness(genes[i]))
-
+    scoret.append(fitness(genes[i]))
+score = normalize(scoret)
 for q in range(niter):
     children = []
     children_score = []
     # score = score/np.sum(score)
     for i in range(int(n/2)):
-        print(score)
         x = np.random.choice(n, 2, p=score/np.sum(score))
         a, b = crossover(genes[x[0]],genes[x[1]])
-        print(100)
         if child_option == 0:
              if nx.is_empty(a)==False and nx.is_empty(b)==False:
                 while(nx.is_connected(a)==False):
                     a = mutation(a)
-                    print('here1')
+                    # print('here1')
                 while(nx.is_connected(b)==False):
                     b = mutation(b)
-                    print('here2')
+                    # print('here2')
                 children.append(a)
                 children.append(b)
                 children_score.append(fitness(a))
@@ -233,19 +252,25 @@ for q in range(niter):
                     children_score.append(fitness(b))
             
     # genes = list(genes)
-    score = list(score)
+    # scoret = list(scoret)
     genes.extend(children)
-    score.extend(children_score)
+    scoret.extend(children_score)
+    score = normalize(scoret)
     genes = [x for _,x in sorted(zip(score,genes), key = lambda x: x[0], reverse = True)][:n]
+    scoret = [x for _,x in sorted(zip(score,scoret), key = lambda x: x[0], reverse = True)][:n]
     score = sorted(score , reverse = True)[:n]
     for i in range(n):
         x = np.random.choice(100)
         if x < mutation_rate:
-            t = mutation(G)
+            try:
+                t = mutation2(G)
+            except FunctionTimedOut:    
+                t = G    
             while(nx.is_connected(t)==False):
                 t = mutation(t)            
             genes[i] = t
-            score[i] = fitness(genes[i])
-    print('here')
+            scoret[i] = fitness(genes[i])
+            score = normalize(scoret)
+    print(score)
 print(score)
 print(genes)
