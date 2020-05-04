40f3de92b8fb896ceaa68cda6b6fe68ecf4ac5b3
Vaikunth Dharwadkar <47127942+vdharwa1@users.noreply.github.com>
2020-05-03 22:49:40 -0400

    Initial commit

---
+++ GCP_Files/setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install vim
+sudo apt-get install python3-pip
+pip3 install networkx
+pip3 install numpy
+pip3 install matplotlib
+pip3 install func_timeout
+pip3 install timeout_decorator
